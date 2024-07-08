81621f7d81deba91c448f39c36a380c1bff5d057
Maksymilian <dsw52686@dsw.edu.pl>
2024-07-08 14:15:45 +0200

    dodanie pliku .sh

---
+++ komendy.sh
@@ -0,0 +1,90 @@
+git commit
+git branch bugFix
+git checkout bugFix
+git branch bugFix
+git checkout bugFix
+git commit
+git checkout main
+git commit
+git merge bugFix
+git branch bugFix
+git checkout bugFix
+git commit
+git checkout main
+git commit
+git checkout bugFix
+git rebase main
+git checkout C4
+git checkout C3
+git checkout HEAD^
+git rebase -i caption~2 --aboveAll
+git commit --amend
+git rebase -i caption~2 --aboveAll
+git branch -f master caption
+git branch -f main C6
+git branch -f bugFix HEAD^
+git reset HEAD^
+git checkout pushed
+git revert pushed
+git cherry-pick C3 C4 C7
+git checkout main
+git cherry-pick bugFix
+git checkout master
+git cherry-pick C2
+git commit --amend
+git cherry-pick C3
+git tag v0 C1
+git tag v1 C2
+git checkout C2
+git commit
+git rebase main bugFix
+git rebase bugFix side
+git rebase side another
+git rebase another main
+git branch bugWork main~^2~
+git checkout one
+git cherry-pick C4 C3 C2
+git checkout two
+git cherry-pick C5 C4 C3 C2
+git branch -f three C2
+git clone
+git commit
+git checkout o/master
+git commit
+git fetch
+git pull
+git clone
+git fakeTeamwork master 2
+git commit
+git pull
+git clone
+git commit
+git commit
+git push
+git clone
+git fakeTeamwork
+git commit
+git pull --rebase
+git push
+git branch feature
+git checkout feature
+git branch -f main c1
+git push 
+git checkout main
+git pull --rebase
+git cherry-pick c2 c3 c4 c5 c6 c7
+git push
+git checkout main
+git pull
+git merge side1
+git merge side2
+git merge side3
+git push
+git checkout -b side o/main
+git commit
+git pull --rebase
+git push
+git push origin foo:main
+git push origin main^:foo
+git push origin :foo
+git fetch origin :bar
